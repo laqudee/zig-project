71d773920ef69db1dba2abf0c0361d029da12b5c
laqudee <2268678583@qq.com>
2023-07-18 22:27:44 +0800

    feat doc-example chapter-10 code again

--- doc-example/chapter-10/run.zig
+++ doc-example/chapter-10/run.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
 const expect = std.testing.expect;
+const expectError = std.testing.expectError;
 const mem = std.mem;
 const builtin = std.builtin;
 
@@ -126,6 +127,16 @@ test "switch simple" {
     try expect(sw(200) == 9);
 }
 
+test "condition is enum or tagged union" {
+    const uni = union(enum) { a1: u32, a2: i64, a3: u32, b: bool };
+    const i = uni{ .b = false };
+    switch (i) {
+        .a1, .a3 => {},
+        uni.a2 => {},
+        .b => try expect(!i.b),
+    }
+}
+
 pub fn main() void {
     var i: i32 = 0;
     while (i < 5) : (i += 1) {
@@ -139,4 +150,226 @@ pub fn main() void {
     }) {
         print("{} {}\n", .{ i, j });
     }
+
+    labeled_loop();
 }
+fn labeled_loop() void {
+    var i: i32 = 0;
+    outer1: while (true) {
+        while (i < 5) : (i += 1) {
+            print("loop.1 i={} \n", .{i});
+            if (i == 1) break :outer1;
+        }
+        print("break out,not output\n", .{});
+    }
+    i = 0;
+    outer2: while (i < 3) : (i += 1) {
+        var j: i32 = i + 1;
+        while (j >= 0) : (j -= 1) {
+            print("loop.2 i={},j={}\n", .{ i, j });
+            if (j == 1) continue :outer2;
+        }
+        print("continue skip,not output\n", .{});
+    }
+}
+
+test "capture v" {
+    const a: i32 = 5;
+    const r = switch (a) {
+        0...4 => false,
+        else => |v| if (v == 5) true else false,
+    };
+    try expect(r);
+}
+
+test "capture index" {
+    const a = [_]i32{ 11, 1, 33, 44 };
+    for (a, 0..) |v, i| {
+        if (v == i) {
+            try expect(i == 1);
+        }
+    }
+}
+
+test "capture pointer" {
+    var a = [_]i32{ 1, 2, 3, 4 };
+    for (&a) |*v| {
+        v.* += 10;
+    }
+    const r = [_]i32{ 11, 12, 13, 14 };
+    for (a, 0..) |v, i| {
+        try expect(v == r[i]);
+    }
+}
+
+test "capture in switch" {
+    var a = [_]i32{ 11, 22, 33, 44 };
+    try expect(foo(&a, 0) == 11);
+    try expect(foo(&a, 1) == 22);
+    try expect(foo(&a, 2) == 43);
+    try expect(a[2] == 43);
+}
+fn foo(addr: *[4]i32, b: usize) i32 {
+    switch (addr[b]) {
+        0...30 => |v| return v,
+        31...50 => |*v| {
+            v.* += 10;
+            return v.*;
+        },
+        else => return 100,
+    }
+}
+
+test "if" {
+    const i: ?i32 = 5;
+    if (i) |v| {
+        try expect(v == 5);
+    } else {
+        try expect(false);
+    }
+
+    const j: ?u32 = null;
+    if (j) |_| {
+        unreachable;
+    } else {
+        try expect(j == null);
+    }
+}
+
+test "while" {
+    var i: ?u32 = 5;
+    var num: i32 = 0;
+    while (i) |*v| : (num += 1) {
+        if (v.* == 2) {
+            i = null;
+        } else {
+            v.* -= 1;
+        }
+    } else {
+        try expect(i == null);
+        try expect(num == 4);
+    }
+}
+
+test "if error union capture" {
+    var a: anyerror!i32 = 1;
+    if (a) |v| {
+        try expect(v == 1);
+    } else |_| {
+        unreachable;
+    }
+
+    if (a) |v| {
+        try expect(v == 1);
+    } else |_| {}
+    a = error.BadValue;
+    if (a) |_| {} else |e| {
+        try expect(e == error.BadValue);
+    }
+}
+
+test "while error union capture" {
+    var i: anyerror!u32 = 5;
+    var num: i32 = 0;
+    while (i) |*v| : (num += 1) {
+        if (v.* == 2) {
+            i = error.BadValue;
+        } else {
+            v.* -= 1;
+        }
+    } else |e| {
+        try expect(e == error.BadValue);
+        try expect(i == error.BadValue);
+        try expect(num == 4);
+    }
+}
+
+test "if capture values type is optional" {
+    const a: anyerror!?i32 = 1;
+    if (a) |v| {
+        try expect(v.? == 1);
+    } else |_| {}
+}
+
+test "if a pointer capture" {
+    var a: anyerror!?i32 = 1;
+    if (a) |*v| {
+        v.*.? = 2;
+    } else |_| {}
+    try expect((a catch @as(i32, 0)).? == 2);
+}
+
+test "if null" {
+    const a: anyerror!?i32 = null;
+    if (a) |v| {
+        try expect(v == null);
+    } else |_| {}
+}
+test "if error" {
+    const a: anyerror!?i32 = error.BadValue;
+    if (a) |_| {} else |e| {
+        try expect(e == error.BadValue);
+    }
+}
+
+test "while error union" {
+    var i: anyerror!?u32 = 5;
+    var num: i32 = 0;
+    while (i) |*v| : (num += 1) {
+        if (v.*.? == 2) {
+            i = error.BadValue;
+        } else {
+            v.*.? -= 1;
+        }
+    } else |e| {
+        try expect(e == error.BadValue);
+        try expect(i == error.BadValue);
+        try expect(num == 4);
+    }
+}
+
+test "inline while loop" {
+    comptime var i = 0;
+    var sum: usize = 0;
+    inline while (i < 3) : (i += 1) {
+        const T = switch (i) {
+            0 => f32, // 3 => 2**3
+            1 => i8, // 2 => 2**2
+            2 => bool, // 4 => 2**4
+            else => unreachable,
+        };
+        print("{}.len: {} \n", .{ T, @typeName(T).len });
+        sum += @typeName(T).len;
+    }
+    try expect(sum == 9);
+}
+
+test "inline for loop" {
+    const nums = [_]i32{ 2, 4, 6 };
+    var sum: usize = 0;
+    inline for (nums) |v| {
+        const T = switch (v) {
+            2 => f32,
+            4 => i8,
+            6 => bool,
+            else => unreachable,
+        };
+        sum += @typeName(T).len;
+    }
+    try expect(sum == 9);
+}
+
+// 不可达unreachable
+test "basic math" {
+    const x = 1;
+    const y = 2;
+    if (x + y != 3) {
+        unreachable;
+    }
+}
+// fn assert(ok: bool) void {
+//     if (!ok) unreachable;
+// }
+// test "this will fail" {
+//     assert(false);
+// }
