7f4147c9d7ee3b222ee7511c400905d63fc383cd
Laqudee Des <24524492+laqudee@users.noreply.github.com>
2023-08-14 09:29:02 +0800

    feat zig webAssembly code  wasi

---
+++ zig-webAssembly/wasi_args.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+    const args = try std.process.argsAlloc(gpa);
+    defer std.process.argsFree(gpa, args);
+
+    for (args, 0..) |arg, i| {
+        std.debug.print("{}: {s}\n", .{ i, arg });
+    }
+}
+
+// use
+// zig build-exe wasi_args.zig -target wasm32-wasi
+// wasmtime  wasi_args.wasm 123 hello
---
+++ zig-webAssembly/wasi_preopens.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const fs = std.fs;
+
+pub fn main() !void {
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+
+    var arena_instance = std.heap.ArenaAllocator.init(gpa);
+    defer arena_instance.deinit();
+    const arena = arena_instance.allocator();
+
+    const preopens = try fs.wasi.preopensAlloc(arena);
+
+    for (preopens.names, 0..) |preopen, i| {
+        std.debug.print("{}: {s}\n", .{ i, preopen });
+    }
+}
+
+// use
+// zig build-exe wasi_preopens.zig -target wasm32-wasi
+// wasmtime --dir=. wasi_preopens.wasm
